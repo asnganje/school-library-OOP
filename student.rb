31a6f01dda22223dd7fa8c92f4c178a0147a7f5e
asnganje <abd.nganje@gmail.com>
2022-11-09 20:40:11 +0300

    add student class

---
+++ student.rb
@@ -0,0 +1,13 @@
+require './person'
+class Student < Person
+  attr_accessor :classroom
+
+  def initialize(classroom)
+    super(name, age, parent_permission)
+    @classroom = classroom
+  end
+
+  def play_hooky
+    "¯\(ツ)/¯"
+  end
+end
