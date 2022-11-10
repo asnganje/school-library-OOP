3c22dba4230ef3906b21b1cb1225b255b7265a8a
asnganje <abd.nganje@gmail.com>
2022-11-10 16:39:50 +0300

    fix linter erros

--- classroom.rb
+++ classroom.rb
@@ -1,13 +1,13 @@
 class Classroom
-    attr_accessor :label
-    attr_reader :students
+  attr_accessor :label
+  attr_reader :students
 
-    def initialize(@label)
-        @label = label
-        @student = []
-    end
+  def initialize(label)
+    @label = label
+    @student = []
+  end
 
-    def add_student(student)
-        @students.push(student) unless @students.include?(student)
-    end
+  def add_student(student)
+    @students.push(student) unless @students.include?(student)
+  end
 end
