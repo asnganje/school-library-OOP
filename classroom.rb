98f18a763d49d2aa06e2482ba9045a729028b348
asnganje <abd.nganje@gmail.com>
2022-11-10 17:18:22 +0300

    Fix has-many' side (a classroom has many students)

--- classroom.rb
+++ classroom.rb
@@ -1,13 +1,13 @@
 class Classroom
   attr_accessor :label
-  attr_reader :students
 
   def initialize(label)
+    @students = []
     @label = label
-    @student = []
   end
 
   def add_student(student)
-    @students.push(student) unless @students.include?(student)
+    @students << student
+    student.classroom = self
   end
 end
